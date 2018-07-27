0db500a3ceaff5cb45a2a0cd5deed04bbde78211
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-27 22:40:49 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,18 +1,24 @@
 class Artist
-  attr_accessor :name, :total_songs
 
+attr_accessor :name,
 
-def initialize(song)
-@name
-song = song.artist
-
-# return total_songs
-end
+def initialize
+  @name
+  
 end
 
-
-#       has many songs (FAILED - 3)
-#     #add_song
-#     .song_count
-#       is a class method that returns the total number of songs associated to all existing a
-# rtists (FAILED - 6)
+#new
+   is initialized with a name (FAILED - 1)
+ #name
+   has an attr_accessor for name (FAILED - 2)
+   #songs
+     has many songs (FAILED - 3)
+   #add_song
+     takes in an argument of a song and associates that song with the artist by telling th
+e song that it belongs to that artist (FAILED - 4)
+   #add_song_by_name
+     takes in an argument of a song name, creates a new song with it and associates the so
+ng and artist (FAILED - 5)
+   .song_count
+     is a class method that returns the total number of songs associated to all existing a
+rtists (FAILED - 6)
