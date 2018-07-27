209cc10b24342e6aae5c85272b6cbe9baee60288
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-27 21:09:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,14 +20,14 @@ else
 
 end
 
-
-
-        is a class method that returns an array of all song instances that have been created (F
-    AILED - 21)
-      #name
-        has a name (FAILED - 22)
-      #artist
-        belongs to an artist (FAILED - 23)
-      #artist_name
-        knows the name of its artist (FAILED - 24)
-        returns nil if the song does not have an artist (FAILED - 25)
+    # 
+    # 
+    #     is a class method that returns an array of all song instances that have been created (F
+    # AILED - 21)
+    #   #name
+    #     has a name (FAILED - 22)
+    #   #artist
+    #     belongs to an artist (FAILED - 23)
+    #   #artist_name
+    #     knows the name of its artist (FAILED - 24)
+    #     returns nil if the song does not have an artist (FAILED - 25)
