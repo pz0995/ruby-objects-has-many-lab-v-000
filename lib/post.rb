90fe21685a3fbcff1254f946512520c59dd8ea05
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-27 22:13:15 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -24,21 +24,21 @@ end
 
 
 
-# pushes new instances into a class variable called @@all upon initialization (FAILED - 1
-# 3)
-# @@all
-#  is a class variable set to an array
-# .all
-#  is a class method that returns an array of all post instances that have been created (F
-# AILED - 14)
-# #title
-#  has a title (FAILED - 15)
-# #author
-#  belongs to an author (FAILED - 16)
-# #author_name
-#  knows the name of its author (FAILED - 17)
-# #
-# #
+pushes new instances into a class variable called @@all upon initialization (FAILED - 1
+3)
+@@all
+ is a class variable set to an array
+.all
+ is a class method that returns an array of all post instances that have been created (F
+AILED - 14)
+#title
+ has a title (FAILED - 15)
+#author
+ belongs to an author (FAILED - 16)
+#author_name
+ knows the name of its author (FAILED - 17)
+#
+#
 #     is a class method that returns an array of all post instances that have been created (F
 # AILED - 16)
 #   #title
