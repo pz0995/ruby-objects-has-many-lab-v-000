9429a2a0f12348fdee654631551a1c8f9c17b6cc
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-27 22:48:37 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,14 +8,14 @@ def initialize
 end
 
 
-   #songs
-     has many songs (FAILED - 3)
-   #add_song
-     takes in an argument of a song and associates that song with the artist by telling th
-e song that it belongs to that artist (FAILED - 4)
-   #add_song_by_name
-     takes in an argument of a song name, creates a new song with it and associates the so
-ng and artist (FAILED - 5)
-   .song_count
-     is a class method that returns the total number of songs associated to all existing a
-rtists (FAILED - 6)
+#    #songs
+#      has many songs (FAILED - 3)
+#    #add_song
+#      takes in an argument of a song and associates that song with the artist by telling th
+# e song that it belongs to that artist (FAILED - 4)
+#    #add_song_by_name
+#      takes in an argument of a song name, creates a new song with it and associates the so
+# ng and artist (FAILED - 5)
+#    .song_count
+#      is a class method that returns the total number of songs associated to all existing a
+# rtists (FAILED - 6)
